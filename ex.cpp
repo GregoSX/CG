bac875b556e4089ff296b18a667e19d5a9994edd
GregoSX <guilhermegrego376@gmail.com>
2022-12-08 10:00:18 -0300

    commit

--- ex.cpp
+++ ex.cpp
@@ -1,117 +1,103 @@
-#include <stdlib.h>
 #include <GL/glut.h>
-#include <iostream>
+#include <stdlib.h>
 
-using namespace std;
+float opacTorus = 1.0;
+float opacCone = 1.0;
+float opacSphere = 1.0;
 
-void drawCasa() {
-    glBegin( GL_LINES ); 
-        glColor3f(0.0f,0.0f,1.0f); 
-        glVertex3f(0.0f,0.0f,0.0f); 
-        glVertex3f(0.7f,0.0f,0.0f); 
-        glColor3f(1.0f,0.0f,0.0f); 
-        glVertex3f(0.0f,0.0f,0.0f); 
-        glVertex3f(0.0f,0.5f,0.0f); 
-        glColor3f(0.0f,1.0f,0.0f); 
-        glVertex3f(0.0f,0.0f,0.0f); 
-        glVertex3f(0.0f,0.0f,0.7f); 
-    glEnd(); 
+void display() {
+   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glColor3f(1.0f,1.0f,1.0f); 
-    glBegin( GL_LINE_STRIP ); 
-        glVertex3f(0.0f,0.2f,0.4f); 
-        glVertex3f(0.0f,0.2f,0.0f); 
-        glVertex3f(0.2f,0.3f,0.0f); 
-        glVertex3f(0.4f,0.2f,0.0f); 
-        glVertex3f(0.4f,0.0f,0.0f); 
-        glVertex3f(0.4f,0.0f,0.4f); 
-    glEnd(); 
+   glPushMatrix ();
+   glRotatef (20.0, 1.0, 0.0, 0.0);
 
-    glBegin( GL_LINE_LOOP ); 
-        glVertex3f(0.0f,0.0f,0.4f); 
-        glVertex3f(0.0f,0.2f,0.4f); 
-        glVertex3f(0.2f,0.3f,0.4f); 
-        glVertex3f(0.4f,0.2f,0.4f); 
-        glVertex3f(0.4f,0.0f,0.4f); 
-    glEnd(); 
+   glPushMatrix ();
+   glTranslatef (-0.75, 0.5, 0.0); 
+   glRotatef (90.0, 1.0, 0.0, 0.0);
+   glColor4f (1.0, 1.0, 0.0, opacTorus);
+   glutSolidTorus (0.275, 0.85, 15, 15);
+   glPopMatrix ();
 
-    glBegin( GL_LINES ); 
-        glVertex3f(0.2f,0.3f,0.0f); 
-        glVertex3f(0.2f,0.3f,0.4f); 
-        glVertex3f(0.4f,0.2f,0.0f); 
-        glVertex3f(0.4f,0.2f,0.4f); 
-    glEnd();
+   glPushMatrix ();
+   glTranslatef (-0.75, -0.5, 0.0); 
+   glRotatef (270.0, 1.0, 0.0, 0.0);
+   glColor4f (0.0, 1.0, 0.0, opacCone);
+   glutSolidCone (1.0, 2.0, 15, 15);
+   glPopMatrix ();
+
+   glPushMatrix ();
+   glTranslatef (0.75, 0.0, -1.0); 
+   glColor4f (0.0, 0.0, 1.0, opacSphere);
+   glutSolidSphere (1.0, 15, 15);
+   glPopMatrix ();
+
+   glPopMatrix ();
+   glFlush ();
 }
 
-void Desenha() {
-    glClear( GL_COLOR_BUFFER_BIT );
-
-    drawCasa();
-
-    glFlush();
+void reshape(int w, int h) {
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   if (w <= h) 
+      glOrtho (-2.5, 2.5, -2.5*(GLfloat)h/(GLfloat)w, 
+               2.5*(GLfloat)h/(GLfloat)w, -10.0, 10.0);
+   else 
+      glOrtho (-2.5*(GLfloat)w/(GLfloat)h, 
+               2.5*(GLfloat)w/(GLfloat)h, -2.5, 2.5, -10.0, 10.0);
+   glMatrixMode (GL_MODELVIEW);
+   glLoadIdentity ();
 }
 
-void recortar() {
-    GLdouble eqn[4] = {0.0, 1.0, 0.0, 0.0};
-    GLdouble eqn2[4] = {1.0, 0.0, 0.0, 0.0};
-
-    glPushMatrix();
-    glTranslatef (0.0, 0.0, -5.0);
-
-    glClipPlane (GL_CLIP_PLANE0, eqn);
-    glEnable (GL_CLIP_PLANE0);
-    glClipPlane (GL_CLIP_PLANE1, eqn2);
-    glEnable (GL_CLIP_PLANE1);
-
-    glRotatef (90.0, 1.0, 0.0, 0.0);
-    drawCasa();
-    glPopMatrix();
+void keyboard(unsigned char key, int x, int y) {
+   switch (key) {
+      case 27:
+         exit(0);
+      break;
+      case 'q':
+         if(opacTorus <= 1.0)
+            opacTorus += 0.1f;
+      break;
+      case 'w':
+         if(opacTorus >= 0.0)
+            opacTorus -= 0.1f;
+      break;
+      case 'a':
+         if(opacCone <= 1.0)
+            opacCone += 0.1f;
+      break;
+      case 's':
+         if(opacCone >= 0.0)
+            opacCone -= 0.1f;
+      break;
+      case 'z':
+         if(opacSphere <= 1.0)
+            opacSphere += 0.1f;
+      break;
+      case 'x':
+         if(opacSphere >= 0.0)
+            opacSphere -= 0.1f;
+      break;
+   }
+   glutPostRedisplay();
 }
 
-void Teclado (unsigned char key, int x, int y) {
-	switch (key) {
-        case 27:
-            exit(0);
-        break;
-        case 'r':
-            recortar();
-        break;
-        default:
-            cout << "Comando inválido " << endl;
-        break;
-    }
-    glutPostRedisplay();
+void init() {
+   glEnable(GL_BLEND);
+   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+   glShadeModel(GL_FLAT);
+   glClearColor(0.0, 0.0, 0.0, 0.0);
 }
 
-void reshape (int w, int h) {
-    glViewport(0,0,w,h); 
-    glMatrixMode( GL_PROJECTION ); 
-    glLoadIdentity(); 
-    gluPerspective(30.0,w/h,1.0,10.0); 
-    glMatrixMode( GL_MODELVIEW ); 
-    glLoadIdentity(); 
-    glTranslatef(0.0f,0.0f,-3.0f); 
-    glRotatef(30.0f,1.0f,0.0f,0.0f); 
-    glRotatef(-45,0.0f,1.0f,0.0f); 
-}
-
-void init(void) {
-   glClearColor (0.0, 0.0, 0.0, 0.0);
-   glShadeModel (GL_FLAT);
-}
-
-
-int main(int argc, char* argv[]) {
-    glutInit(&argc, argv);
-	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize (500, 500); 
-    glutInitWindowPosition (100, 100);
-	glutCreateWindow("Exercício Prático 02 - 1ª parte");
-    init();
-	glutDisplayFunc(Desenha);
-    glutReshapeFunc(reshape); 
-	glutKeyboardFunc(Teclado);
-	glutMainLoop();
-
-	return 0;
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);
+   glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH | GLUT_RGBA);
+   glutInitWindowSize (500, 500);
+   glutCreateWindow("Exercício Prático 03 - 1ª parte");
+   init();
+   glutReshapeFunc (reshape);
+   glutDisplayFunc(display);
+   glutKeyboardFunc (keyboard);
+   glutMainLoop();
+   return 0; 
 }
